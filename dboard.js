326b6d245e7960d2734544d5c5331ca7ffcb881c
Safia Zahid <safiaxahid@gmail.com>
2026-05-11 06:07:04 +0500

    Time & greeting update

--- dboard.js
+++ dboard.js
@@ -2,15 +2,22 @@ var backgroundImg = ""
 function addPost() {
   var title = document.getElementById("title");
   var description = document.getElementById("description");
-  console.log(title.value, description.value);
+  //console.log(title.value, description.value);
+  var descText = description.innerHTML;
 
-  if (title.value.trim() && description.value.trim()) {
+  if (title.value.trim() && descText.trim()) {
     var posts = document.getElementById("post-container");
     posts.innerHTML = `
     <div class="card mb-3">
                     <div class="card-header">
                         Dear ${userLocalData.name}<br>
-                        ${moment().format('DD MMMM YYYY')}
+                       <span style="
+        font-family: 'Poppins', sans-serif;
+        font-size: 13px;
+        letter-spacing: 0.5px
+        font-weight: 900;
+        
+    ">${moment().format('DD MMMM YYYY, hh : mm A')}</span>
                     </div>
                     <div style ="background-image:url(${backgroundImg}) "  class="card-body">
                         <figure>
@@ -18,7 +25,7 @@ function addPost() {
                                 <p>${title.value}</p>
                             
                             <footer class="blockquote-footer">
-                                ${description.value}
+                                ${descText}
                             </footer>
                             </blockquote>
                         </figure>
@@ -26,7 +33,7 @@ function addPost() {
                 </div>` + posts.innerHTML;
                 renderPost();
     title.value = "";
-    description.value = "";
+    description.innerHTML = "";
   } else {
     Swal.fire({
       icon: "error",
@@ -51,11 +58,12 @@ function renderPost(){
   var title = document.getElementById("title");
   var description = document.getElementById("description");
   var post = document.getElementById("post-container");
+  var descText = description.innerHTML;
 
-  if (title.value.trim() && description.value.trim() ){
+  if (title.value.trim() && descText.trim() ){
     var postObj = {
       title : title.value,
-      description: description.value,
+      description:descText,
       image : backgroundImg
 
     };
@@ -73,8 +81,30 @@ function redirection(){
 function logOut(){
 setTimeout(redirection, 500);
 }
+function setGreeting(){
+  let hour = moment().hour();
+  let greeting ="";
+
+  if (hour >= 5 && hour < 12){
+greeting = "Good Morning";
+  }
+  else if (hour >= 12 && hour < 17){
+    greeting = "Good Afternoon";
+
+  }
+  else if (hour >= 17 && hour < 23){
+    greeting = "Good Evening";
+    
+  }
+  else {
+    greeting = "Good Night";
+    
+  }
+  document.getElementById("greet").innerText = greeting + ", "+ userLocalData.name;
+}
 
 window.onload = function(){
+  setGreeting();
   var allPosts = JSON.parse(localStorage.getItem("posts")) || [];
   var posts = document.getElementById("post-container");
 
@@ -99,4 +129,11 @@ window.onload = function(){
                 </div>` + posts.innerHTML;
   }
 }
+function format(command, value = null){
+  document.execCommand(command, false, value);
+}
+// let colorPicker = document.getElementById("colorPicker");
+// colorPicker.addEventListener("input", function(){
+//   document.execCommand("foreColor", false, this.value);
+// } )
 
